278bedffc6c8aff3cffdff0be177a7d4e9881601
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-02-05 23:01:14 +0100

    Add: more extensive tests for Span class

--- cpp08/ex01/src/main.cpp
+++ cpp08/ex01/src/main.cpp
@@ -1,4 +1,6 @@
 #include "../inc/Span.hpp"
+#include <cstddef>
+#include <exception>
 
 void	subjectMain()
 {
@@ -10,6 +12,28 @@ void	subjectMain()
 	sp.addNumber(11);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
+}
+
+void	trySpans(Span span)
+{
+	try
+	{
+		size_t i = span.shortestSpan();
+		std::cout << "shortestSpan:\t[" << i << "]" << std::endl;
+	}
+	catch (const std::exception &nospan)
+	{
+		std::cerr << nospan.what() << " - Size: [" << span.getN() << "]" << std::endl;
+	}
+	try
+	{
+		size_t i = span.longestSpan();
+		std::cout << "longestSpan:[" << i << "]" << std::endl;
+	}
+	catch (const std::exception &nospan)
+	{
+		std::cerr << nospan.what() << " - Size: [" << span.getN() << "]" << std::endl;
+	}
 }
 
 void	trySmall()
@@ -31,8 +55,7 @@ void	trySmall()
 			alreadyFull = true;
 		}
 	}
-	std::cout << "small.shortestSpan():\t[" << small.shortestSpan() << "]" << std::endl;
-	std::cout << "small.longestSpan():\t[" << small.longestSpan() << "]\n" << std::endl;
+	trySpans(small);
 }
 
 void	tryHuge()
@@ -54,13 +77,39 @@ void	tryHuge()
 			alreadyFull = true;
 		}
 	}
-	std::cout << "huge.shortestSpan():\t[" << huge.shortestSpan() << "]" << std::endl;
-	std::cout << "huge.longestSpan():\t[" << huge.longestSpan() << "]\n" << std::endl;
+	trySpans(huge);
+}
+
+void	tryEmpty()
+{
+	Span empty(0);
+	bool alreadyFull = false;
+	int	value = 0;
+
+	while (alreadyFull == false)
+	{
+		try
+		{
+			empty.addNumber(value);
+			value++;
+		}
+		catch (const std::exception &full)
+		{
+			std::cerr << full.what() << " - Size: [" << empty.getN() << "]" << std::endl;
+			alreadyFull = true;
+		}
+	}
+	trySpans(empty);
 }
 
 int main()
 {
+	std::cout << "--- Testing With Small Container ---\n" << std::endl;
 	trySmall();
+	std::cout << "\n--- Testing With Big Ass Container ---\n" << std::endl;
 	tryHuge();
+	std::cout << "\n--- Testing With Empty Container ---\n" << std::endl;
+	tryEmpty();
+	std::cout << "\n--- Main From Subject ---\n" << std::endl;
 	subjectMain();
 }
