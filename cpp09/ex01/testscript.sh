32e6313ce855068e87f324a4bf5511299605e809
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-02-06 14:51:50 +0100

    Update testscript.sh

--- cpp09/ex01/testscript.sh
+++ cpp09/ex01/testscript.sh
@@ -12,12 +12,6 @@ do
     expected_exit_code="${parts[2]}"
     actual_output=$(./RPN "$testcase")
     exit_code=$?
-    boldred="\033[1;31m"
-    boldgreen="\033[1;32m"
-    boldyellow="\033[1;33m"
-    boldblue="\033[1;34m"
-    reset="\033[0m"
-    italic="\033[3m"
     bold="\033[1m"
     printf "$bold\"$testcase\"$reset\nExpected: $expected_output - Result: $actual_output\n"
 
