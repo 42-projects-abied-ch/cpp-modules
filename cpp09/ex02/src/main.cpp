794671256f4a936a00e41bbe316a2e18868a45eb
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-02-13 11:57:16 +0000

    this is looking like something now

--- cpp09/ex02/src/main.cpp
+++ cpp09/ex02/src/main.cpp
@@ -1,17 +1,31 @@
 #include "../inc/PmergeMe.hpp"
 
+std::string	errorMessage(const std::string &message)
+{
+	return "error: " + message;
+}
+
+void	PmergeMe::verifyInput(int argc, char **argv)
+{
+	if (argc < 2)
+		throw PmergeMe::Exception(errorMessage("not enough arguments"));
+	for (int i = 1; i < argc; i++)
+	{
+		if (std::string(argv[i]).find_first_not_of("0123456789") != std::string::npos)
+			throw PmergeMe::Exception(errorMessage("invalid element"));
+		v_mergeMe.push_back(std::atoi(argv[i]));
+	}
+}
+
 int main(int argc, char **argv)
 {
 	try
+  	{
+		PmergeMe mergeMeBaby(argc, argv);
+		mergeMeBaby.v_sort();
+  	}
+  	catch (const std::exception &e)
 	{
-		verifyInput(argc, argv);
-	}
-	catch (const std::exception &e)
-	{
-		std::cerr << e.what() << std::endl;
-		return EXIT_FAILURE;
-	}
-	PmergeMe mergeMeBaby;
-	mergeMeBaby.v_FordJohnson(argc, argv);
-	mergeMeBaby.d_FordJohnson(argc, argv);
+		std::cout << e.what() << std::endl;
+  	}
 }
